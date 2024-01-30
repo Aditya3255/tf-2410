9807d4d13ecf855b50840d2d2a9f1f5941070384
aditya3255 <adityasantosh464@gmail.com>
2024-01-30 21:09:32 +0530

    create web subnet

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,15 @@ resource "aws_vpc" "myntra_vpc" {
     Name = "myntra"
   }
 }
+
+#web subnet
+resource "aws_subnet" "myntra_websn" {
+  vpc_id     = aws_vpc.myntra-vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch ="true"
+
+  tags = {
+    Name = "Myntra-web-subnet"
+  }
+}
