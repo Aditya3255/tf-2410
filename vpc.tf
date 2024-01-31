269e9b6f631ea80630e63e1a975389a05fa8bd1b
aditya3255 <adityasantosh464@gmail.com>
2024-01-31 09:40:17 +0530

    create database subnet

--- vpc.tf
+++ vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "myntra_vpc" {
 }
 
 #web subnet
-resource "aws_subnet" "myntra_websn" {
+resource "aws_subnet" "myntra-web-sn" {
   vpc_id     = aws_vpc.myntra-vpc.id
   cidr_block = "10.0.0.0/24"
   availability_zone = "ap-south-1a"
@@ -17,5 +17,16 @@ resource "aws_subnet" "myntra_websn" {
 
   tags = {
     Name = "Myntra-web-subnet"
+  }
+}
+#Database subnet
+resource "aws_subnet" "myntra-db-sn" {
+  vpc_id     = aws_vpc.myntra-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch ="false"
+
+  tags = {
+    Name = "Myntra-database-subnet"
   }
 }
