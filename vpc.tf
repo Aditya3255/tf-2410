22bc6d8e0a5a4b523c0898f796436145497343dc
aditya3255 <adityasantosh464@gmail.com>
2024-01-31 10:10:14 +0530

    create igw

--- vpc.tf
+++ vpc.tf
@@ -9,24 +9,33 @@ resource "aws_vpc" "myntra_vpc" {
 }
 
 #web subnet
-resource "aws_subnet" "myntra-web-sn" {
-  vpc_id     = aws_vpc.myntra-vpc.id
+resource "aws_subnet" "myntra_web_sn" {
+  vpc_id     = aws_vpc.myntra_vpc.id
   cidr_block = "10.0.0.0/24"
   availability_zone = "ap-south-1a"
   map_public_ip_on_launch ="true"
 
   tags = {
-    Name = "Myntra-web-subnet"
+    Name = "Myntra_web_subnet"
   }
 }
 #Database subnet
-resource "aws_subnet" "myntra-db-sn" {
-  vpc_id     = aws_vpc.myntra-vpc.id
+resource "aws_subnet" "myntra_db_sn" {
+  vpc_id     = aws_vpc.myntra_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
   map_public_ip_on_launch ="false"
 
   tags = {
-    Name = "Myntra-database-subnet"
+    Name = "Myntra_database_subnet"
+  }
+}
+
+#internet gateway
+resource "aws_internet_gateway" "myntra_igw" {
+  vpc_id = aws_vpc.myntra_vpc.id
+
+  tags = {
+    Name = "myntra_internet_gateway"
   }
 }
