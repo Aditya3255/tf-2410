13fe92caa2dd3ec8738a485f0edcc7504cd5da76
aditya3255 <adityasantosh464@gmail.com>
2024-01-31 10:57:54 +0530

    websn asc

--- vpc.tf
+++ vpc.tf
@@ -63,3 +63,8 @@ resource "aws_route_table" "myntra-database-rt" {
     Name = "myntra-database-route-table"
   }
 }
+#websubnet association:
+resource "aws_route_table_association" "myntra-web-asc" {
+  subnet_id      = aws_subnet.myntra-web-sn.id
+  route_table_id = aws_route_table.myntra-web-rt.id
+}
